426750c634c35a197686d593d3d2a88a52898967
CodeSchwert <2661899+CodeSchwert@users.noreply.github.com>
2021-06-22 18:22:26 +1200

    Serve mock data for the client-side React app.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const version = require('./package.json').version;
 const notFoundHandler = require('./middleware/404Handler');
 const errorHandler = require('./middleware/errorHandler');
+const data = require('./mockData/db.json');
 
 // constants
 const PORT = process.env.SERVER_PORT || 5500;
@@ -13,7 +14,7 @@ const PORT = process.env.SERVER_PORT || 5500;
 const server = express();
 
 // middleware
-server.use(cors());
+server.use(cors({ origin: '*', methods: ['GET', 'POST', 'PATCH', 'DELETE'] }));
 server.use(helmet());
 server.use(express.json());
 server.use(morgan('dev'));
@@ -21,6 +22,17 @@ server.use(morgan('dev'));
 // server version / debug route
 server.get('/', (_, res) => {
   return res.json({ service: 'teaching-availability', version });
+});
+
+server.get('/availability', (req, res) => {
+  return res.status(200).json(data.availability);
+});
+
+server.patch('/availability/:id', (req, res) => {
+  return res.status(200).json({
+    params: req.params,
+    body: req.body
+  });
 });
 
 // error handlers
