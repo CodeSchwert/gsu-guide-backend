621c7f9386112c96bf257e5769926072ae3b2854
CodeSchwert <2661899+CodeSchwert@users.noreply.github.com>
2021-06-22 21:35:01 +1200

    Refactor data repo methods, add POST route to create new events.

--- data/repos/availability.js
+++ data/repos/availability.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
 const Availability = require('../models/availabilitySchema');
 
-const getAvailability = async () => {
+const queryAvailability = async (query={}) => {
   try {
     const pipeline = [
-      { $match: {} },                   // match/find all documents
+      { $match: query },                // match/find all documents
       { $addFields: { id: '$_id' } },   // alias id field from _id
       { $project: { _id: 0, __v: 0 } }  // don't return _id or __v
     ];
@@ -16,8 +15,32 @@ const getAvailability = async () => {
     console.error(e);
     throw e;
   }
+}
+
+const getAvailability = () => {
+  const results = queryAvailability();
+  return results;
+};
+
+const addAvailability = async (event) => {
+  try {
+    if (!event) {
+      throw new Error('Availability event required.');
+    }
+
+    // TODO -- Add Joi object validation on `event`
+
+    const newEvent = await Availability.create(event);
+    const results = queryAvailability({ _id: newEvent._id });
+  
+    return results;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 module.exports = {
-  getAvailability
+  getAvailability,
+  addAvailability
 };
--- routers/availability.js
+++ routers/availability.js
@@ -10,6 +10,25 @@ const availabilityRouter = (dataRepo) => {
     return res.status(200).json(availabilityEvents);
   });
 
+  router.post('/', async (req, res, next) => {
+    try {
+      const event = req.body;
+
+      if (!event) {
+        return res
+          .status(400)
+          .json({ error: 'Availability event object required.' });
+      }
+
+      const newEvent = await dataRepo.addAvailability(event);
+
+      return res.status(201).json(newEvent);
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  });
+
   router.patch('/:id', (req, res) => {
     return res.status(200).json({
       params: req.params,
