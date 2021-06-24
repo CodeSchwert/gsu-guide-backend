d021b3707b871a6e8432b61ff6d360954b189785
CodeSchwert <2661899+CodeSchwert@users.noreply.github.com>
2021-06-24 20:57:06 +1200

    Remove extra array.

--- utils/mock-availability.js
+++ utils/mock-availability.js
@@ -1,40 +1,38 @@
 module.exports = [
-  [
-    {
-      title: 'Learn how to play Magic the Gathering',
-      start: '2021-06-21T21:00:00.000Z',
-      end: '2021-06-22T03:00:00.000Z',
-      timezone: 'Pacific/Auckland'
-    },
-    {
-      title: 'Learn how to play Pokemon TCG',
-      start: '2021-06-22T23:30:00.000Z',
-      end: '2021-06-23T07:30:00.000Z',
-      timezone: 'Pacific/Auckland'
-    },
-    {
-      title: 'Learn how to play Flesh and Blood',
-      start: '2021-06-23T19:30:00.000Z',
-      end: '2021-06-24T09:30:00.000Z',
-      timezone: 'Pacific/Auckland'
-    },
-    {
-      title: 'Learn how to play Magic the Gathering',
-      start: '2021-06-28T21:00:00.000Z',
-      end: '2021-06-29T03:00:00.000Z',
-      timezone: 'Pacific/Auckland'
-    },
-    {
-      title: 'Learn how to play Pokemon TCG',
-      start: '2021-06-29T23:30:00.000Z',
-      end: '2021-06-30T07:30:00.000Z',
-      timezone: 'Pacific/Auckland'
-    },
-    {
-      title: 'Learn how to play Flesh and Blood',
-      start: '2021-06-30T19:30:00.000Z',
-      end: '2021-07-01T09:30:00.000Z',
-      timezone: 'Pacific/Auckland'
-    }
-  ]
+  {
+    title: 'Learn how to play Magic the Gathering',
+    start: '2021-06-21T21:00:00.000Z',
+    end: '2021-06-22T03:00:00.000Z',
+    timezone: 'Pacific/Auckland'
+  },
+  {
+    title: 'Learn how to play Pokemon TCG',
+    start: '2021-06-22T23:30:00.000Z',
+    end: '2021-06-23T07:30:00.000Z',
+    timezone: 'Pacific/Auckland'
+  },
+  {
+    title: 'Learn how to play Flesh and Blood',
+    start: '2021-06-23T19:30:00.000Z',
+    end: '2021-06-24T09:30:00.000Z',
+    timezone: 'Pacific/Auckland'
+  },
+  {
+    title: 'Learn how to play Magic the Gathering',
+    start: '2021-06-28T21:00:00.000Z',
+    end: '2021-06-29T03:00:00.000Z',
+    timezone: 'Pacific/Auckland'
+  },
+  {
+    title: 'Learn how to play Pokemon TCG',
+    start: '2021-06-29T23:30:00.000Z',
+    end: '2021-06-30T07:30:00.000Z',
+    timezone: 'Pacific/Auckland'
+  },
+  {
+    title: 'Learn how to play Flesh and Blood',
+    start: '2021-06-30T19:30:00.000Z',
+    end: '2021-07-01T09:30:00.000Z',
+    timezone: 'Pacific/Auckland'
+  }
 ];
